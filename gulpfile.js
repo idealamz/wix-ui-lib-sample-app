aeb45260ce4bb1cab89a691f447c7e4bf4609d31
Adiel <adiela@wix.com>
2015-12-28 12:19:39 +0200

    save

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 var gulp = require('gulp');
 
 require('./tasks/css');
-require('./tasks/reactTemplates');
+require('./tasks/jsx');
 require('./tasks/serve');
 
-gulp.task('default', ['serve', 'css', 'rt'], function () {
+gulp.task('default', ['serve', 'css', 'jsx'], function () {
     gulp.watch('src/**/*.scss', ['css']);
-    gulp.watch('src/**/*.rt', ['rt']);
+    gulp.watch('src/**/*.jsx.js', ['jsx']);
 });
 
---
+++ src/widget/index.jsx.js
@@ -0,0 +1,91 @@
+define(['react', 'lodash', 'Wix'], function (React, _, Wix) {
+    return React.createClass({
+        displayName: 'template',
+        getInitialState: function () {
+            return {
+                wixParams: {},
+                appWixData: {}
+            }
+        },
+        componentDidMount: function () {
+            var _this = this;
+            Wix.setHeight(1200);
+            Wix.Styles.getStyleParams(this.updateWixParams);
+            Wix.addEventListener(Wix.Events.THEME_CHANGE, function (styleObj) {
+                _this.updateWixParams(styleObj.style);
+            });
+            Wix.addEventListener(Wix.Events.SETTINGS_UPDATED, this.getWixData);
+            this.getWixData();
+        },
+        updateWixParams: function (style) {
+            var wixParams = _.assign(
+                style.numbers,
+                style.fonts,
+                style.booleans,
+                style.colors
+            );
+
+            this.setState({
+                wixParams: wixParams
+            }, this.updateCompHeight);
+        },
+        getWixData: function () {
+            var _this = this;
+            Wix.Data.Public.get('appData', function (data) {
+                _this.setState({
+                    appWixData: data.appData
+                }, _this.updateCompHeight);
+            });
+        },
+        updateCompHeight: function () {
+            Wix.setHeight(document.documentElement.scrollHeight);
+        },
+        stringify: function (input) {
+            try {
+                return JSON.stringify(input, null, 4);
+            } catch (err) {
+                return input;
+            }
+        },
+        render: function () {
+            var wixParamRow = function (key) {
+                return (
+                    <tr>
+                        <td>{key}</td>
+                        <td>
+                            <pre>
+                                <code dangerouslySetInnerHTML={{__html: this.stringify(this.state.wixParams[key])}}/>
+                            </pre>
+                        </td>
+                    </tr>
+                );
+            };
+
+            return (
+                <div>
+                    <section id="wix-data-public">
+                        <h3>Wix.Data.Public</h3>
+                        <pre>
+                            <code dangerouslySetInnerHTML={{__html: this.stringify(this.state.appWixData)}}/>
+                        </pre>
+                    </section>
+
+                    <section id="wix-param">
+                        <h3>wix-param values</h3>
+                        <table>
+                            <thead>
+                            <tr>
+                                <th>param name</th>
+                                <th>value</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                                {_.map(Object.keys(this.state.wixParams), wixParamRow.bind(this))}
+                            </tbody>
+                        </table>
+                    </section>
+                </div>
+            );
+        }
+    });
+});
